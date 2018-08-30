4eb1df1128f28eb85f6673f03b948fb42044acee
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-30 23:42:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,11 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|c| c.length >= 4} ? true : false
+  calls.any? {|c| c.length >= 4}
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
