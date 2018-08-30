2ff546ffc0feefcd4a69e2e83ddd38283ccd996f
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-30 23:40:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,6 @@ end
 
 def long_planeteer_calls(calls)
   calls.any? {|c| c.length >= 4}
-  # if calls.any? do |call|
-  #   call.length >= 4
-  # end
 end
 
 def find_the_cheese# code an argument here
